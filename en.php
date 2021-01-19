1be090a3201b1d3b99e8afcdba4b38600e2b64b7
Emie Lafourcade <77435337+LafourcadeEmie@users.noreply.github.com>
2021-01-19 14:39:25 +0100

    Add files via upload

---
+++ en.php
@@ -0,0 +1,19 @@
+<?php 
+$etudes="Studies";
+$competences="Skills";
+$projets="Projects";
+
+$intro="Welcome to my resume. Thank you for taking the time to read it.<Br />
+I am looking for a 10 to 12 weeks internship in order to complete my bachelor's degree in computer science. <Br />
+Next year I would like to do a master's degree in artificial intelligence.<Br /> </p>
+<p class=\"fr\">Resume in <a href=\"./images/LAFOURCADE_Emie_CVpdf\" target=\"blank\">PDF</a>";
+
+$presentation = "Hi! <Br />
+My name is Emie Lafourcade, <Br />
+I am 20 years old, <Br />
+I have a driver's licence, <Br />
+and I live in Villeurbanne, France. <Br /><Br />
+I am a young and motivated programmer who wants to learn.
+Easygoing and curious, I am never afraid to ask questions to always improve myself.
+However I am still an autonomous and independante worker but I also like to work with other people.";
+?>
---
+++ fr.php
@@ -0,0 +1,19 @@
+<?php 
+$etudes="Etudes";
+$competences="Compétences";
+$projets="Projets";
+
+$intro="Bienvenu sur mon CV. Merci d'avoir pris le temps de le consulter. <Br />
+Je suis à la recherche d'un stage de 10 à 12 semaines pour valider ma licence en informatique. <Br />
+L'année prochaine je souhaite réaliser un master en intelligence artificielle.<Br /> </p>
+<p class=\"fr\">CV au format <a href=\"./images/LAFOURCADE_Emie_CVpdf\" target=\"blank\">PDF</a>";
+
+$presentation = "Bonjour ! <Br />
+Je m'appelle Emie Lafourcade, <Br />
+j'ai 20 ans, <Br />
+j'ai le permis B, <Br />
+et j'habite à Villeurbanne. <Br /><Br />
+Je suis une jeune programmeuse très motivée, qui a envie d'apprendre.
+Sociable et curieuse, je n'hésite pas à poser des questions pour toujours m'améliorer.
+Je reste cependant indépendante dans mon travail mais aime aussi travailler en groupe.";
+?>
